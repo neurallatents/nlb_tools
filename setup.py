e101e84b92b08a75191313a02004adbee112a2fe
felixp8 <felp8484@gmail.com>
2023-07-10 18:30:49 -0400

    update version number also

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nlb_tools',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'pandas>=1.0.0,<=1.3.4',
